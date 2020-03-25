a5fe914db54a729d44a2a48191424d632bc0aa63
Dino <dino.pith@gmail.com>
2020-03-25 15:22:46 +0100

    Adding MVC folders

---
+++ CountOnMe/Model/Brain.swift
@@ -0,0 +1,14 @@
+//
+//  Brain.swift
+//  CountOnMe
+//
+//  Created by pith on 25/03/2020.
+//  Copyright © 2020 Vincent Saluzzo. All rights reserved.
+//
+
+import Foundation
+
+class Brain {
+
+    
+}
