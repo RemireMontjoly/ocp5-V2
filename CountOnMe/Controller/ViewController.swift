4d5c97b146bf8f8f69b1ce80a0bcfa5f6055509b
Dino <dino.pith@gmail.com>
2020-03-31 09:07:57 +0200

    Adding class Brain in Model

--- CountOnMe/Controller/ViewController.swift
+++ CountOnMe/Controller/ViewController.swift
@@ -9,8 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+
     @IBOutlet weak var textView: UITextView!
     @IBOutlet var numberButtons: [UIButton]!
+      private var brain = Brain()
     
     var elements: [String] {
         return textView.text.split(separator: " ").map { "\($0)" }
@@ -43,70 +45,74 @@ class ViewController: UIViewController {
     // View actions
     @IBAction func tappedNumberButton(_ sender: UIButton) {
         guard let numberText = sender.title(for: .normal) else {
-            return
-        }
-        
-        if expressionHaveResult {
-            textView.text = ""
-        }
-        
-        textView.text.append(numberText)
+             return
+         }
+         if brain.calculateFinished {
+             // Clear the display and add numberDisplay.last value
+             brain.addNewNumber(newNumber: numberText)
+             textView.text = brain.numberDisplay.last
+         } else {
+             brain.addNewNumber(newNumber: numberText)
+             textView.text.append(numberText)
+         }
     }
     
     @IBAction func tappedAdditionButton(_ sender: UIButton) {
-        if canAddOperator {
-            textView.text.append(" + ")
-        } else {
-            let alertVC = UIAlertController(title: "Zéro!", message: "Un operateur est déja mis !", preferredStyle: .alert)
-            alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
-            self.present(alertVC, animated: true, completion: nil)
-        }
+        brain.addPlusOperator()
+         //Todo: if check is ok then:
+        textView.text.append("+")
+//        if canAddOperator {
+//            textView.text.append(" + ")
+//        } else {
+//            let alertVC = UIAlertController(title: "Zéro!", message: "Un operateur est déja mis !", preferredStyle: .alert)
+//            alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+//            self.present(alertVC, animated: true, completion: nil)
+//        }
     }
     
     @IBAction func tappedSubstractionButton(_ sender: UIButton) {
-        if canAddOperator {
-            textView.text.append(" - ")
-        } else {
-            let alertVC = UIAlertController(title: "Zéro!", message: "Un operateur est déja mis !", preferredStyle: .alert)
-            alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
-            self.present(alertVC, animated: true, completion: nil)
-        }
+        brain.addPlusOperator()
+          //Todo: if check is ok then:
+         textView.text.append("-")
+//        if canAddOperator {
+//            textView.text.append(" - ")
+//        } else {
+//            let alertVC = UIAlertController(title: "Zéro!", message: "Un operateur est déja mis !", preferredStyle: .alert)
+//            alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+//            self.present(alertVC, animated: true, completion: nil)
+//        }
     }
 
+    @IBAction func tappedDivideButton(_ sender: UIButton) {
+        brain.addDivideOperator()
+         textView.text.append(":")
+    }
+
+    @IBAction func tappedMultiplierButton(_ sender: UIButton) {
+        brain.addMultiplierOperator()
+        textView.text.append("x")
+    }
+
+
+
+
+
     @IBAction func tappedEqualButton(_ sender: UIButton) {
-        guard expressionIsCorrect else {
-            let alertVC = UIAlertController(title: "Zéro!", message: "Entrez une expression correcte !", preferredStyle: .alert)
-            alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
-            return self.present(alertVC, animated: true, completion: nil)
-        }
-        
-        guard expressionHaveEnoughElement else {
-            let alertVC = UIAlertController(title: "Zéro!", message: "Démarrez un nouveau calcul !", preferredStyle: .alert)
-            alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
-            return self.present(alertVC, animated: true, completion: nil)
-        }
-        
-        // Create local copy of operations
-        var operationsToReduce = elements
-        
-        // Iterate over operations while an operand still here
-        while operationsToReduce.count > 1 {
-            let left = Int(operationsToReduce[0])!
-            let operand = operationsToReduce[1]
-            let right = Int(operationsToReduce[2])!
-            
-            let result: Int
-            switch operand {
-            case "+": result = left + right
-            case "-": result = left - right
-            default: fatalError("Unknown operator !")
-            }
-            
-            operationsToReduce = Array(operationsToReduce.dropFirst(3))
-            operationsToReduce.insert("\(result)", at: 0)
-        }
-        
-        textView.text.append(" = \(operationsToReduce.first!)")
+        brain.calculate()
+        textView.text = brain.resultExpression
+        //Todo: check error
+
+//        guard expressionIsCorrect else {
+//            let alertVC = UIAlertController(title: "Zéro!", message: "Entrez une expression correcte !", preferredStyle: .alert)
+//            alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+//            return self.present(alertVC, animated: true, completion: nil)
+//        }
+//
+//        guard expressionHaveEnoughElement else {
+//            let alertVC = UIAlertController(title: "Zéro!", message: "Démarrez un nouveau calcul !", preferredStyle: .alert)
+//            alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+//            return self.present(alertVC, animated: true, completion: nil)
+//        }
     }
 
 }
--- CountOnMe/Model/Brain.swift
+++ CountOnMe/Model/Brain.swift
@@ -10,5 +10,100 @@ import Foundation
 
 class Brain {
 
-    
+    var calculateFinished = true
+    var resultExpression = ""
+    var stringExpression = [String]()
+    var numberDisplay = [""]
+
+    func addNewNumber(newNumber: String) {
+        // calculateFinished = false
+
+        if var stringMutable = numberDisplay.last {
+            stringMutable += newNumber
+            numberDisplay = [stringMutable]
+            print("Add number: \(numberDisplay)")
+        }
+    }
+    func addPlusOperator() {
+        //Todo: check error
+
+        //        if stringExpression.last == "+" || stringExpression.last == "-" || stringExpression.last == "/" || stringExpression.last == "*" {
+        //            let alertVC = UIAlertController(title: "Zéro!", message: "Un operateur est déja mis !", preferredStyle: .alert)
+        //            alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+        //            self.present(alertVC, animated: true, completion: nil)
+        //        }
+        stringExpression += numberDisplay
+        stringExpression.append("+")
+        numberDisplay = [""]
+        calculateFinished = false
+        print("String Expression: \(stringExpression)")
+
+    }
+    func addMinusOperator() {
+        //Todo: check error
+
+        stringExpression += numberDisplay
+        stringExpression.append("-")
+        numberDisplay = [""]
+        calculateFinished = false
+        print(stringExpression)
+    }
+    func addDivideOperator() {
+        //Todo: check error
+
+        stringExpression += numberDisplay
+        stringExpression.append("/")
+        numberDisplay = [""]
+        calculateFinished = false
+        print(stringExpression)
+    }
+    func addMultiplierOperator() {
+        //Todo: check error
+
+        stringExpression += numberDisplay
+        stringExpression.append("*")
+        numberDisplay = [""]
+        calculateFinished = false
+        print(stringExpression)
+    }
+
+    func calculate() {
+        stringExpression += numberDisplay
+        numberDisplay = [""]
+        print(stringExpression)
+
+        for (i,oprator) in stringExpression.enumerated() {
+            if oprator == "/" || oprator == "*" {
+
+                let leftIndex = i - 1
+                let rightIndex = i + 1
+
+                print("leftIndex = \(leftIndex)")
+                print("RightIndex = \(rightIndex)")
+
+                let left = Int(stringExpression[leftIndex])!
+                let operand = stringExpression[i]
+                let right = Int(stringExpression[rightIndex])!
+
+                print("left = \(left)")
+                print("right = \(right)")
+                print("operand = \(operand)")
+
+                let result: Int
+                switch operand {
+                case "/": result = left / right
+                case "*": result = left * right
+                default: fatalError("Unknown operator !")
+
+                }
+                stringExpression = Array(stringExpression.dropFirst(2))
+                 stringExpression[0] = String(result)
+
+
+                print("The result = \(result)")
+                print(stringExpression)
+            }
+
+        }
+    }
 }
